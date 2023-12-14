13bc2f0da167db0f7e0781544a5258683d3844c6
rauw <rauw@ubuntu.myguest.virtualbox.org>
2023-12-14 19:18:06 +0100

    ultimo commit actualizado desde casa de rauw

--- 01-react-tiempo/src/assets/imagenes.jsx
+++ 01-react-tiempo/src/assets/imagenes.jsx
@@ -1,3 +1,5 @@
+//vamsos a crear este componente para pode utlizar imagenes y de manera más sencilla las utilizaremos como objetos
+
 import lluvioso from './img/estadosTiempo/lluvioso.png'
 
 export default {
