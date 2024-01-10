d98a8b1c89e8cf3235c0addf11f847bba39b19e2
Juan <ospinajuanandres8@gmail.com>
2024-01-10 14:10:38 +0100

    Boton reset implementado

--- 02-react-3EnRaya/src/components/darClick.jsx
+++ 02-react-3EnRaya/src/components/darClick.jsx
@@ -1,24 +1,6 @@
-import { useState } from "react";
-export default function Square(){
-    const [value,setValue]=useState(Array(9).fill(null));
+
     
-    function darClick(index){
-      const darValor=value.map(c,i =>{
-        if(i===index){
-          return 'x';
-        }else{
-          return c;
-        }
-      })
-      }
-      return(
-        <button className="square"
-        onClick={darClick}>
-          {value}
-        </button>
-      )
-    
-  }
+  
   
 
 
