4480d4e48b7b65d4ae0337ae83c7e9f9e91dc8e9
rauw <cdm.raulportillo@gmail.com>
2024-01-17 14:15:13 +0100

    ultima actualizacion

--- Pokedex_Rauw/src/App.jsx
+++ Pokedex_Rauw/src/App.jsx
@@ -1,17 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { BuscarPOkemon } from './componentes/Formulario'
+import { CartaForm } from './componentes/Carta'
+import { Pokemons } from './componentes/Pokemons'
+import React from "react"
+
+
+const api = {
+  base: "https://pokeapi.co/api/v2/pokemon"
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [buscar, setBuscar] = useState('')
+  const [Pokedex, setPokemon] = useState('')
+
+  const buscarPressed = () => {
+    fetch(`${api.base}/${buscar}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setPokemon(result);
+        console.log(result)
+      });
+
+  }
+  const[listPokemon, setListPokemon] = useState([])
+  useEffect(() => {
+    const getListPokemon = async () => {
+
+      const url = await fetch(`https://pokeapi.co/api/v2/pokemon/kanto`)
+      const listaPokemon = await url.json()
+      const { pokemon_entries } = listaPokemon
+
+
+      const listaPokemonResult = pokemon_entries.map(async (pokemon) => {
+        const auxPokemon = await fetch(`${pokemon.pokemon_name}`)
+        const resultPokemon = await auxPokemon.json()
+
+        return resultPokemon
+      })
+
+      const resultadoCompleto = await Promise.all(listaPokemonResult).then( resultados => resultados.map((result) => {
+
+      }))
+      
+      setListPokemon(resultadoCompleto)
+    }
+    getListPokemon()
+  }, [])
 
   return (
     <>
-      <BuscarPOkemon>
-        
-      </BuscarPOkemon>
+      <BuscarPOkemon setBuscar={setBuscar} buscarPressed={() => buscarPressed}></BuscarPOkemon>
+      <CartaForm Pokedex={Pokedex}></CartaForm>
     </>
   )
 }
---
+++ Pokedex_Rauw/src/componentes/Pokemons.jsx
@@ -0,0 +1,4 @@
+
+export function Pokemons({listPokemon}){
+
+}
