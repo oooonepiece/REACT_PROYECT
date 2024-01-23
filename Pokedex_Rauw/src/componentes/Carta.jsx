f6deefe6201df2e5ed273c170ccceaac36a25310
rauw374 <150252835+rauw374@users.noreply.github.com>
2024-01-23 19:27:06 +0100

    actualizacion

--- Pokedex_Rauw/src/componentes/Carta.jsx
+++ Pokedex_Rauw/src/componentes/Carta.jsx
@@ -1,13 +1,13 @@
 export function CartaForm({Pokedex}) {
     return (
         <div className=" row mx-auto ">
-            <div className="card bg-secondary bg-opacity-25 text-white">
+            <div className="card bg-black bg-opacity-25 text-white">
                 {typeof Pokedex.name != "undefined" ?
-                    <img src={Pokedex.sprites.other["official-artwork"].front_default} className=" w-25 h-75  mx-auto " />
+                    <img src={Pokedex.sprites.other["official-artwork"].front_default} className=" w-25 h-75  mx-auto rounded-circle bg-danger px-4 py-4  mt-4 mb-4 border border-black " />
                     :
                     ' '
                 }
-               <h1 className="mx-auto mt-4">{Pokedex.name}</h1>
+               <h1 className="mx-auto rounded-pill border border-danger px-5 py-3 bg-black mt-4">{Pokedex.name}</h1>
                
             </div>
         </div>
