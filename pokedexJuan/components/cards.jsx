85d9d251d7ed80e68d7fa8c51879fa18ad10ad10
KlkJuan <150250911+KlkJuan@users.noreply.github.com>
2024-02-06 20:58:58 +0100

    movimientos

--- pokedexJuan/components/cards.jsx
+++ pokedexJuan/components/cards.jsx
@@ -1,27 +1,28 @@
 
+
 export function CartaPokemon({Pokemon}) {
    
     return (
+        <>
+        
+        
         <div className="row row-cols-6   mt-2 ">
             {
                 Pokemon.map((poke) => {
                     return (
-                        <div className="card bg-black bg-opacity-75 text-white mt-5  ms-5  ">
+                        <div key={poke.id} className="card bg-black bg-opacity-75 text-white mt-5  ms-5  ">
                             <p className="card-text  w-50 mx-auto  mt-4 text-center ">Id. {poke.id} </p>
                             <div className="card-body">
                             <img className="h-100 w-100 " src={poke.sprites.other["official-artwork"].front_default}></img>
                             <p className="card-text  w-50 mx-auto  mt-2 text-center ">{poke.name} </p>
                             </div>
-                            <div className="card-img-top h-100">
-                           
-                                    
-                               
-                               
-                            </div>
+                            <div className="card-img-top h-100">       
+                           </div>
                         </div>
                     )
                 })
             }
         </div>
+       </>
     )
 }
